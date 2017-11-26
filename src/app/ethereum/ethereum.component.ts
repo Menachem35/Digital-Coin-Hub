d17cfe84e5a0f79c1c42f6bc25afa3b55acd5778
Menachem Glik <menachemglik@yahoo.com>
2017-11-26 02:29:17 -0500

    Create ethereum.component.ts

---
+++ src/app/ethereum/ethereum.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+import { DCHlinks } from '../links'; //DCHlinks class 
+import { DigitalCoinHubService } from '../digital-coin-hub.service';
+
+@Component({
+	selector: 'ethereum-section',
+	templateUrl: './ethereum.component.html'
+})
+
+export class EthereumComponent implements OnInit {
+	
+	constructor(private appLinksService: DigitalCoinHubService) { }
+	
+	title = 'Ethereum';
+	
+	links: DCHlinks[]; // create a new array in type of DCHlinks to get the data
+  
+    getLinks(): void {
+	   this.links = this.appLinksService.getLinksByCategory('Ethereum'); // the array above (links), gets the array with the data from the service
+    }
+  
+    ngOnInit() {
+	   this.getLinks();   
+    }
+}
