750b96dac020b0312e4b6347106d18371d2e620f
Menachem Glik <menachemglik@yahoo.com>
2017-11-26 11:49:19 -0500

    Create jobs.component.ts

---
+++ src/app/jobs/jobs.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+import { DCHlinks } from '../links'; //DCHlinks class 
+import { DigitalCoinHubService } from '../digital-coin-hub.service';
+
+@Component ({
+	selector: 'jobs',
+	templateUrl: 'jobs.component.html'
+})
+
+export class JobsComponent implements OnInit {
+	
+	constructor(private appLinksService: DigitalCoinHubService) { }
+	
+	title = 'Jobs';
+	
+	links: DCHlinks[]; // create a new array in type of DCHlinks to get the data
+  
+    getLinks(): void {
+		this.links = this.appLinksService.getLinksByCategory('jobs'); // the array above (links), gets the array with the data from the service
+    }
+  
+	ngOnInit() {
+		this.getLinks();   
+	}
+}
