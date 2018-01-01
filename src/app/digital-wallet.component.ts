db3008a49a28cb54c7ea051e4969345ed1357ea6
Menachem Glik <menachemglik@yahoo.com>
2018-01-01 00:56:14 -0500

    Delete digital-wallet.component.ts

--- src/app/digital-wallet.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-	selector: 'digital-wallet', 
-	templateUrl:  './digital-wallet.component.html'
-})
-
-export class DigitalWalletComponent {
-}
