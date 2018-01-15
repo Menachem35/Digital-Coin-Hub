668e55dd31e233668e17bb01f2cbce2a78767df4
Menachem Glik <menachemglik@yahoo.com>
2018-01-15 00:45:01 -0500

    Update digital-coins.component.ts

--- src/app/digital-coins/digital-coins.component.ts
+++ src/app/digital-coins/digital-coins.component.ts
@@ -2,17 +2,24 @@ import { Component, OnInit } from '@angular/core';
 
 import { DCHlinks } from '../links'; //DCHlinks class 
 import { DigitalCoinHubService } from '../digital-coin-hub.service';
+import { DataDisplayFromAPI } from '../data-display-from-api.service';
 
 @Component ({
 	selector: 'digital-coins',
-	templateUrl: 'digital-coins.component.html'
+	templateUrl: 'digital-coins.component.html',
+	styleUrls: ['./digital-coins.component.css']
 })
 
 export class DigitalCoinsComponent implements OnInit {
 	
-	constructor(private appLinksService: DigitalCoinHubService) { }
+	constructor(private appLinksService: DigitalCoinHubService, private coinsRateFromService: DataDisplayFromAPI) { }
 	
 	title = 'Digital Coins';
+	
+	objectKeys = Object.keys; // Get data from API through service 
+    cryptos: any; // Get data from API through service
+	
+	coinsToDisplay = 'BTC,ETH,XMR,BTG,BCH,LTC,XRP,IOT,XVG,XRB,FCT,XLM,XZC,ZEC,DASH,ADA,TRX,DBC,EMB,MKR,FNC';
 	
 	links: DCHlinks[]; // create a new array in type of DCHlinks to get the data
   
@@ -21,6 +28,12 @@ export class DigitalCoinsComponent implements OnInit {
 	}
   
 	ngOnInit() {
-		this.getLinks();   
+		this.getLinks(); 
+			
+		this.coinsRateFromService.getPrices(this.coinsToDisplay)
+			.subscribe(res => {
+				this.cryptos = res;
+			});
 	}
 }
+
